70f5255284191a32868b1c72c54757c4714321df
shreyanshbhatt <shreyanshpbhatt@gmail.com>
2018-08-07 18:22:55 -0400

    finishing pipeline and a few related changes in tl_classifier. tested with highway track. looks like it is working (#4)

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,11 @@
+import rospy
 from styx_msgs.msg import TrafficLight
 import rospkg
 import os,sys
 import tensorflow as tf
 import numpy as np
-
+import time
+import os
 
 def load_graph (graph_file):
     # Load the frozen inference protobuf file 
@@ -18,13 +20,22 @@ def load_graph (graph_file):
 
 
 class TLClassifier(object):
-    def __init__(self, path_to_model):
+    def __init__(self, is_site=False):
 
-'''
        #TODO load classifier
        self.tf_session = None
        self.prediction = None
-       self.path_to_model = path_to_model
+       self.path_to_model = '../../../deep_learning/models/frozen_graphs/'
+       # ros_root = rospkg.get_ros_root()
+       # Not using the following for now to locate the classification model
+       # using absolute path since the models are in deep_learning/....
+       # If we decide to put the models in 'tl_detector' then we would use the following
+       detect_path = rospkg.RosPack().get_path('tl_detector')
+       if is_site:
+           self.path_to_model += 'real_mobilenets_ssd_38k_epochs_frozen_inference_graph.pb'
+       else:
+           self.path_to_model += 'sim_mobilenets_ssd_30k_epochs_frozen_inference_graph.pb'
+       rospy.loginfo('model going to be loaded from'+self.path_to_model)
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -36,10 +47,10 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
+        start_time = time.time()
+
         #TODO implement light color prediction
- 
-        ros_root = rospkg.get_ros_root()
-        detect_path = rospkg.Rospack.get_path('tl_detector')
+        # ros_root = rospkg.get_ros_root()
         
         # Setup tensorflow config and classification
         # When we are startinig..setup the tensorflow configuration
@@ -54,7 +65,8 @@ class TLClassifier(object):
             self.config.operation_timeout_in_ms = 50000 
 
             # load the graph using the path to model file
-            self.tf_graph = load_graph(detect_path + self.path_to_model + '.pb')
+            # path_to_model has the absolute path
+            self.tf_graph = load_graph(self.path_to_model)
 
 
             with self.tf_graph.as_default():
@@ -91,9 +103,7 @@ class TLClassifier(object):
             ## Need to chose from the classes if the num_detections > 1 and also implement
             ## a criteria to set for prediction
             predict = classes[0]
+            #TrafficLight messages have red = 0, yellow = 1, green = 2. The model is trained to identify class red = 1, yellow = 2, green = 3. Hence, subtracting 1 to match with TrafficLight message spec.
+            predict = predict - 1
 
         return predict
- '''
-
-
-        return TrafficLight.UNKNOWN
